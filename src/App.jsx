159508ab4298a1d45404037419acfb42c9c61557
fethulmubin <fetihul@gmail.com>
2025-04-20 22:50:54 +0300

    files opened

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-
+import Navbar from './components/NavBar/NavBar'
+import Hero from './components/Hero/Hero'
+import About from './components/About/About'
+import Projects from './components/Projects/Projects'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 function App() {
-
   return (
-    <div>
-       <h1>start</h1>
-    </div>
+    <>
+      {/* <Navbar /> */}
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
   )
 }
 
