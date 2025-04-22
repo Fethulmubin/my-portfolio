cee4fe63132d672c0f7506675091d8ac1e117724
fethulmubin <fetihul@gmail.com>
2025-04-22 16:54:53 +0300

    3D model o hero added, lightning adjusted

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
-// import Hero from './components/Hero/Hero'
-// import NavBar from './components/NavBar/NavBar'
-// import About from './components/About/About'
-// import Footer from './components/Footer/Footer'
-// import Projects from './components/Projects/Projects'
-// import Contact from './components/Contact/Contact'
+import Hero from './sections/Hero/Hero'
 
 const App = () => {
   return (
-    <main className=''>
-        
-    </main>
+    <>
+      <Hero/>  
+    </>
   )
 }
 
