6929401bb86e90d37e601a54ac2250ea602ba38d
fethulmubin <fetihul@gmail.com>
2025-04-29 12:11:02 +0300

    bulb for mobile responsive

--- src/components/Bulb.jsx
+++ src/components/Bulb.jsx
@@ -6,8 +6,8 @@ import { useMediaQuery } from "react-responsive";
 
 export default function Bulb() {
     const [isBulbOn, setIsBulbOn] = useState(false);
-    const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
-    const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
+    const isMobile = useMediaQuery({ query: "(width >= 768px)" });
+    const isTablet = useMediaQuery({ query: "(width >= 1024px)" });
 
 
     const handleToggle = () => {
@@ -20,17 +20,16 @@ export default function Bulb() {
             <img
                 src={isBulbOn ? bulbOn : bulbOff}
                 alt="Bulb"
-                className={isBulbOn ? " bg-[#fceb55] rounded-full border-4 shadow-amber-600 shadow-2xl transition-all duration-500 size-50" : " bg-amber-50/45 rounded-full transition-all duration-500 size-50"}
+                className={` rounded-full shadow-2xl transition-all duration-500 size-50 ${isBulbOn ? "bg-[#fceb55]  border-[10px] shadow-amber-600 shadow-2xl" : "bg-amber-50/45"} ${isMobile ? "md:w-80 md:h-80 w-60 h-60" : isTablet ? "md:w-80 md:h-80 w-60 h-60" : "w-40 h-40"} `}
             />
 
             {/* String (Wire) */}
             <div
                 onClick={handleToggle}
-                className="relative md:mb-10 mb-80 w-1 h-52 bg-gray-400 cursor-pointer hover:bg-gray-200 mt-2"
+                className={`relative md:mb-10 mb-80 w-1 h-52 bg-gray-400 cursor-pointer hover:bg-gray-200 mt-2`}
             >
-                <div className="absolute top-52 w-4 -right-[6px] bg-white-50 h-10 rounded-full">
+                <div className={`absolute top-52 w-4 -right-[6px] bg-white-50 h-10 rounded-full`}>
                 </div>
-
 
             </div>
 
